9356419d515d39ae1536e20575acd71ea45f19d5
usernames122 <88596366+usernames122@users.noreply.github.com>
2023-08-09 09:59:14 +0200

    Update override.js

--- override.js
+++ override.js
@@ -1,14 +1,3 @@
-// Get a reference to the <body> element
-var body = document.body;
-
-// Remove all child elements except <script> elements
-var childNodes = body.childNodes;
-for (var i = childNodes.length - 1; i >= 0; i--) {
-    var node = childNodes[i];
-    if (node.tagName !== 'SCRIPT') {
-        body.removeChild(node);
-    }
-}
 var body = document.head;
 
 // Remove all child elements except <script> elements
@@ -20,6 +9,17 @@ for (var i = childNodes.length - 1; i >= 0; i--) {
     }
 }
 
+// Get a reference to the <body> element
+var body = document.body;
+
+// Remove all child elements except <script> elements
+var childNodes = body.childNodes;
+for (var i = childNodes.length - 1; i >= 0; i--) {
+    var node = childNodes[i];
+    if (node.tagName !== 'SCRIPT') {
+        body.removeChild(node);
+    }
+}
 // Create and append your own HTML content
 var newDiv = document.createElement('div');
 newDiv.innerHTML = '<p>This is your custom HTML content.</p>';
